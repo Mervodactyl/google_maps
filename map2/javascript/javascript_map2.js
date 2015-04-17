92d4a48fceaddcd12038b1ca4f38d405fa4ecb92
Mervodactyl <merve.silk@gmail.com>
2015-04-17 18:28:53 +0100

    map1 successful

---
+++ map2/javascript/javascript_map2.js
@@ -0,0 +1,7 @@
+var mapOptions = {
+  center: new google.maps.LatLng(51.563296,-0.1074350000000095),
+  zoom: 13,
+  mapTypeId: google.maps.MapTypeId.ROADMAP
+};
+
+new google.maps.Map(document.getElementById("map"), mapOptions);
